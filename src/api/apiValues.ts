1c2a94de0ae04c2f189052dda84d9c9f4d27b01c
Alejandro Meneses <almenesesp@gmail.com>
2023-08-26 15:18:34 -0500

    feat: fetch movie poster image

--- src/api/apiValues.ts
+++ src/api/apiValues.ts
@@ -3,7 +3,12 @@ import {MOVIE_API_KEY} from '@env';
 export const nowPlayingMoviesUrl: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${MOVIE_API_KEY}`;
 export const upcomingMoviesUrl: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${MOVIE_API_KEY}`;
 export const popularMoviesUrl: string = `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API_KEY}`;
+
 export const searchMovieUrl = (searchTerm: string): string =>
   `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${searchTerm}`;
+
 export const movieDetails = (id: number): string =>
   `https://api.themoviedb.org/3/movie/${id}?api_key=${MOVIE_API_KEY}`;
+
+export const moviePoster = (width: number, posterPath: string) =>
+  `http://image.tmdb.org/t/p/w${width}${posterPath}`;
