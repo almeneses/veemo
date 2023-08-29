e0ee05373d7afe229a0f88337a4867bc65fb9208
Alejandro Meneses <almenesesp@gmail.com>
2023-08-29 14:41:59 -0500

    refactor: add horizontal padding to category headers

--- src/components/CategoryHeader.tsx
+++ src/components/CategoryHeader.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     fontFamily: FONTFAMILY.poppins_semibold,
     fontSize: FONTSIZE.s_20,
     paddingVertical: SPACING.s_28,
+    paddingHorizontal: SPACING.s_36,
   },
 });
 
